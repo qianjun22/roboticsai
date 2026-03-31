cde5de88d6190346b0f7e1279ef9866a37bfd785
Jun Qian <qianjun22@users.noreply.github.com>
2026-03-31 16:42:25 -0700

    cycle-11952A: robot_laser_cutter_v39.py + robotics_product_led_v39.py (ports 55838-55839)

--- src/api/robotics_product_led_v39.py
+++ src/api/robotics_product_led_v39.py
@@ -1,5 +1,5 @@
 import datetime,fastapi,fastapi.responses,uvicorn
-PORT=55837
+PORT=55839
 SERVICE="robotics_product_led_v39"
 DESCRIPTION="GTM: product_led"
 app=fastapi.FastAPI(title=SERVICE,version="1.0.0",description=DESCRIPTION)
