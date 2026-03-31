272f58c0ae3f6857561edc28f714124e78f3251d
Jun Qian <qianjun22@users.noreply.github.com>
2026-03-31 16:36:19 -0700

    cycle-11150A: robot_die_attacher_v37.py + robotics_inside_sales_v37.py (ports 52630-52631)

--- src/api/robotics_inside_sales_v37.py
+++ src/api/robotics_inside_sales_v37.py
@@ -1,5 +1,5 @@
 import datetime,fastapi,fastapi.responses,uvicorn
-PORT=52629
+PORT=52631
 SERVICE="robotics_inside_sales_v37"
 DESCRIPTION="GTM: inside_sales"
 app=fastapi.FastAPI(title=SERVICE,version="1.0.0",description=DESCRIPTION)
