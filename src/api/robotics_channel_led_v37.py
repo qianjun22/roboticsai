79d3ca58de5bd72483317b0e7ecf691988187fe3
Jun Qian <qianjun22@users.noreply.github.com>
2026-03-31 16:38:43 -0700

    cycle-11388A: robot_flip_chip_bonder_v37.py + robotics_channel_led_v37.py (ports 53582-53583)

--- src/api/robotics_channel_led_v37.py
+++ src/api/robotics_channel_led_v37.py
@@ -1,5 +1,5 @@
 import datetime,fastapi,fastapi.responses,uvicorn
-PORT=53581
+PORT=53583
 SERVICE="robotics_channel_led_v37"
 DESCRIPTION="GTM: channel_led"
 app=fastapi.FastAPI(title=SERVICE,version="1.0.0",description=DESCRIPTION)
