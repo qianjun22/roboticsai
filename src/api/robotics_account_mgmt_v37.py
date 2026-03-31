ad87875336fd96b9ddfaaed9cacf88d9a71d750b
Jun Qian <qianjun22@users.noreply.github.com>
2026-03-31 16:36:24 -0700

    cycle-11155A: robot_ct_scanner_v37.py + robotics_account_mgmt_v37.py (ports 52650-52651)

--- src/api/robotics_account_mgmt_v37.py
+++ src/api/robotics_account_mgmt_v37.py
@@ -1,5 +1,5 @@
 import datetime,fastapi,fastapi.responses,uvicorn
-PORT=52649
+PORT=52651
 SERVICE="robotics_account_mgmt_v37"
 DESCRIPTION="GTM: account_mgmt"
 app=fastapi.FastAPI(title=SERVICE,version="1.0.0",description=DESCRIPTION)
--- src/simulation/robot_ct_scanner_v37.py
+++ src/simulation/robot_ct_scanner_v37.py
@@ -1,5 +1,5 @@
 import datetime,fastapi,fastapi.responses,uvicorn
-PORT=52530
+PORT=52650
 SERVICE="robot_ct_scanner_v37"
 DESCRIPTION="Sim: ct_scanner"
 app=fastapi.FastAPI(title=SERVICE,version="1.0.0",description=DESCRIPTION)
