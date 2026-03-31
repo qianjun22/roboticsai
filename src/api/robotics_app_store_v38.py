cefd5bb95a55fd9241eafd912f5dbd493ef26313
Jun Qian <qianjun22@users.noreply.github.com>
2026-03-31 16:40:57 -0700

    cycle-11670A: robot_catheter_robot_v38.py + robotics_app_store_v38.py (ports 54710-54711)

--- src/api/robotics_app_store_v38.py
+++ src/api/robotics_app_store_v38.py
@@ -1,5 +1,5 @@
 import datetime,fastapi,fastapi.responses,uvicorn
-PORT=54709
+PORT=54711
 SERVICE="robotics_app_store_v38"
 DESCRIPTION="GTM: app_store"
 app=fastapi.FastAPI(title=SERVICE,version="1.0.0",description=DESCRIPTION)
