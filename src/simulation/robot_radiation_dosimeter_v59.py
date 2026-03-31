e0c4682e58df85627d294851c7508fa5b8eedb74
RoboticsAI Build <jun@roboticsai.dev>
2026-03-31 16:48:14 -0700

    cycle-17739A: robot_radiation_dosimeter_v59.py + robotics_data_room_manager_v59.py (ports 78986-78987)

--- src/simulation/robot_radiation_dosimeter_v59.py
+++ src/simulation/robot_radiation_dosimeter_v59.py
@@ -1,5 +1,5 @@
 import datetime,fastapi,fastapi.responses,uvicorn
-PORT=78886
+PORT=78986
 SERVICE="robot_radiation_dosimeter_v59"
 DESCRIPTION="Sim: radiation_dosimeter"
 app=fastapi.FastAPI(title=SERVICE,version="1.0.0",description=DESCRIPTION)
