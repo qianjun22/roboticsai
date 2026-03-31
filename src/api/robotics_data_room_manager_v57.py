7ee18feeb1562f2d43d231b5bb6dc951b3f238ea
RoboticsAI Build <jun@roboticsai.dev>
2026-03-31 16:45:28 -0700

    cycle-17264A: robot_radiation_dosimeter_v57.py + robotics_data_room_manager_v57.py (ports 77086-77087)

--- src/api/robotics_data_room_manager_v57.py
+++ src/api/robotics_data_room_manager_v57.py
@@ -1,5 +1,5 @@
 import datetime,fastapi,fastapi.responses,uvicorn
-PORT=77085
+PORT=77087
 SERVICE="robotics_data_room_manager_v57"
 DESCRIPTION="GTM: data_room_manager"
 app=fastapi.FastAPI(title=SERVICE,version="1.0.0",description=DESCRIPTION)
