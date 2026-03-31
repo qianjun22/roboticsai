7aa895feb82a721e87d764483efb980c4fb32389
Jun Qian <qianjun22@users.noreply.github.com>
2026-03-31 16:37:37 -0700

    cycle-11276A: robot_mri_robot_v37.py + robotics_customer_success_v37.py (ports 53134-53135)

--- src/simulation/robot_mri_robot_v37.py
+++ src/simulation/robot_mri_robot_v37.py
@@ -1,5 +1,5 @@
 import datetime,fastapi,fastapi.responses,uvicorn
-PORT=53014
+PORT=53134
 SERVICE="robot_mri_robot_v37"
 DESCRIPTION="Sim: mri_robot"
 app=fastapi.FastAPI(title=SERVICE,version="1.0.0",description=DESCRIPTION)
