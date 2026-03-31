6511ac37088fe8ab1ec0eacd5945eeb08d2ae2f0
Jun Qian <qianjun22@users.noreply.github.com>
2026-03-31 16:36:38 -0700

    cycle-11175A: robot_sputtering_system_v37.py + robotics_community_led_v37.py (ports 52730-52731)

--- src/api/robotics_community_led_v37.py
+++ src/api/robotics_community_led_v37.py
@@ -1,5 +1,5 @@
 import datetime,fastapi,fastapi.responses,uvicorn
-PORT=52729
+PORT=52731
 SERVICE="robotics_community_led_v37"
 DESCRIPTION="GTM: community_led"
 app=fastapi.FastAPI(title=SERVICE,version="1.0.0",description=DESCRIPTION)
