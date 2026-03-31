7fbcdafd2cc70949db951e3b2049db90c69bc5c2
Jun Qian <qianjun22@users.noreply.github.com>
2026-03-31 16:39:41 -0700

    cycle-11506A: robot_lithography_robot_v38.py + robotics_developer_led_v38.py (ports 54054-54055)

--- src/api/robotics_developer_led_v38.py
+++ src/api/robotics_developer_led_v38.py
@@ -1,5 +1,5 @@
 import datetime,fastapi,fastapi.responses,uvicorn
-PORT=54053
+PORT=54055
 SERVICE="robotics_developer_led_v38"
 DESCRIPTION="GTM: developer_led"
 app=fastapi.FastAPI(title=SERVICE,version="1.0.0",description=DESCRIPTION)
