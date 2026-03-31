d99c50b9641668752548611db8f6a40d380be85f
Jun Qian <qianjun22@users.noreply.github.com>
2026-03-31 16:43:58 -0700

    cycle-12235A: robot_ct_scanner_v40.py + robotics_account_mgmt_v40.py (ports 56970-56971)

--- src/simulation/robot_ct_scanner_v40.py
+++ src/simulation/robot_ct_scanner_v40.py
@@ -1,5 +1,5 @@
 import datetime,fastapi,fastapi.responses,uvicorn
-PORT=56850
+PORT=56970
 SERVICE="robot_ct_scanner_v40"
 DESCRIPTION="Sim: ct_scanner"
 app=fastapi.FastAPI(title=SERVICE,version="1.0.0",description=DESCRIPTION)
