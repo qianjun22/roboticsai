281728df23f0bde7b22fefb61decc84ef0cbca74
Jun Qian <qianjun22@users.noreply.github.com>
2026-03-31 16:43:01 -0700

    cycle-12071A: robot_optical_tweezer_v40.py + robotics_top_down_v40.py (ports 56314-56315)

--- src/api/robotics_top_down_v40.py
+++ src/api/robotics_top_down_v40.py
@@ -1,5 +1,5 @@
 import datetime,fastapi,fastapi.responses,uvicorn
-PORT=56313
+PORT=56315
 SERVICE="robotics_top_down_v40"
 DESCRIPTION="GTM: top_down"
 app=fastapi.FastAPI(title=SERVICE,version="1.0.0",description=DESCRIPTION)
