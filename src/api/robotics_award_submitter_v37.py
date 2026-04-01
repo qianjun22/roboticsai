c55a65cc076b718a4179e78131be866c27611951
RoboticsAI Build <jun@roboticsai.dev>
2026-03-31 17:14:05 -0700

    cycle-11373A: robot_nuclear_handler_v37.py + robotics_award_submitter_v37.py (ports 53522-53523)

--- src/api/robotics_award_submitter_v37.py
+++ src/api/robotics_award_submitter_v37.py
@@ -1,5 +1,5 @@
 import datetime,fastapi,fastapi.responses,uvicorn
-PORT=53521
+PORT=53523
 SERVICE="robotics_award_submitter_v37"
 DESCRIPTION="GTM: award_submitter"
 app=fastapi.FastAPI(title=SERVICE,version="1.0.0",description=DESCRIPTION)
