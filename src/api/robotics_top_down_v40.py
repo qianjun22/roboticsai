5dda91a58511fcce97f6417a3a7714262ed70f02
RoboticsAI Build <jun@roboticsai.dev>
2026-03-31 17:19:09 -0700

    cycle-12151A: robot_bio_printer_v40.py + robotics_top_down_v40.py (ports 56634-56635)

--- src/api/robotics_top_down_v40.py
+++ src/api/robotics_top_down_v40.py
@@ -1,5 +1,5 @@
 import datetime,fastapi,fastapi.responses,uvicorn
-PORT=56633
+PORT=56635
 SERVICE="robotics_top_down_v40"
 DESCRIPTION="GTM: top_down"
 app=fastapi.FastAPI(title=SERVICE,version="1.0.0",description=DESCRIPTION)
