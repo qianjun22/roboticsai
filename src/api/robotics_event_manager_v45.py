c311f249ae0f242b077b7fd11feacca05b986be1
RoboticsAI Build <jun@roboticsai.dev>
2026-03-31 17:30:39 -0700

    cycle-13768A: robot_prosthetic_ctrl_v45.py + robotics_event_manager_v45.py (ports 63102-63103)

--- src/api/robotics_event_manager_v45.py
+++ src/api/robotics_event_manager_v45.py
@@ -1,5 +1,5 @@
 import datetime,fastapi,fastapi.responses,uvicorn
-PORT=63101
+PORT=63103
 SERVICE="robotics_event_manager_v45"
 DESCRIPTION="GTM: event_manager"
 app=fastapi.FastAPI(title=SERVICE,version="1.0.0",description=DESCRIPTION)
