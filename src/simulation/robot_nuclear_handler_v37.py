1ad361fee805396ea1634083d999a16d9cb5a949
RoboticsAI Build <jun@roboticsai.dev>
2026-03-31 17:13:51 -0700

    cycle-11333A: robot_nuclear_handler_v37.py + robotics_award_submitter_v37.py (ports 53362-53363)

--- src/simulation/robot_nuclear_handler_v37.py
+++ src/simulation/robot_nuclear_handler_v37.py
@@ -1,5 +1,5 @@
 import datetime,fastapi,fastapi.responses,uvicorn
-PORT=53202
+PORT=53362
 SERVICE="robot_nuclear_handler_v37"
 DESCRIPTION="Sim: nuclear_handler"
 app=fastapi.FastAPI(title=SERVICE,version="1.0.0",description=DESCRIPTION)
